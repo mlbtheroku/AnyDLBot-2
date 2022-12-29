48e5641c1cafb8a1d8bdacbaa1bfb640c5279346
mlbtheroku <115041575+mlbtheroku@users.noreply.github.com>
2022-12-30 04:40:14 +0630

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Development(Config):
   API_HASH = "2c015c994c57b312708fecc8a2a0f1a6"
   TG_BOT_TOKEN = "5833550185:AAFzUdwF-uiFxIVwzrVS7VepS5GH9tfv0Bk"
   AUTH_USERS = [
-      "5468192421 5559979635 5504969603 1349301822 5623031120"
+      "5468192421"
       # is a string for LEGACY purposes
   ]
