91eb3fdf8d04cdbd980ffdcdbc3e70c50ae0a4c4
mlbtheroku <115041575+mlbtheroku@users.noreply.github.com>
2022-12-30 04:32:10 +0630

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+from sample_config import Config
+
+class Development(Config):
+  APP_ID = 15830858
+  API_HASH = "2c015c994c57b312708fecc8a2a0f1a6"
+  TG_BOT_TOKEN = "5833550185:AAFzUdwF-uiFxIVwzrVS7VepS5GH9tfv0Bk"
+  AUTH_USERS = [
+      "5468192421 5559979635 5504969603 1349301822 5623031120"
+      # is a string for LEGACY purposes
+  ]
